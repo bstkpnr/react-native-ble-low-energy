e5317681115e59dfa065396b5c7ee3f63167e98b
bstkpnr <bstkpnr01@gmail.com>
2023-04-12 09:59:28 +0300

    create navigation

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+
+
+const Stack=createNativeStackNavigator();
+
+function StackApp(){
+    return(
+        <Stack.Navigator>
+
+        </Stack.Navigator>
+    );
+}
+
+export default StackApp;
